7d6ea243fbda8c34e7eda6d442e0d805a23c1f2c
Tiphaine HOGREL <hogrelt@gmail.com>
2022-07-23 15:15:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {marked} from 'marked';
 import './App.css';
 
 function App() {
+  //state
+  const [text, setText] = useState('Mon texte par défaut');
+  
+  //function
+  const changeTextHandler = event => {
+    setText(event.target.value)
+  };
+  /* I get back the value of my textarea with event.target.value, event = évènement, value = champ, value = valeur*/
+  const markdownToHTML = () => {
+    return {__html: marked(text)};
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1>Prévisualisateur</h1>
+      </div>
+      <div className='elements'>
+        <div className='element'>
+          <textarea
+            rows='30'
+            value= {text}
+            onChange={(e) => changeTextHandler(e)}> 
+          </textarea>
+        </div>
+        <div className='element'>         
+          <div dangerouslySetInnerHTML={markdownToHTML()}></div>
+        </div>
+      </div>     
     </div>
   );
 }
 
 export default App;
+
+/* handler is a standard
+rows='30" is for having 30 line*/
